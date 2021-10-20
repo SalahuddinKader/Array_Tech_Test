c56df7e6668f538144a700e7c35ad5a195ef1db8
SalahuddinKader <salahuddinkader007@gmail.com>
2021-10-20 23:25:22 +0100

    Array_tech_test

---
+++ Index.js
@@ -0,0 +1,5 @@
+import groupArrayElements from './utils/group_array_elements'
+
+console.log("Here is the array [1, 2, 3, 4, 5] passing\r\nthrough the function groupArrayElements and being split into 3 arrays")
+console.log("")
+console.log(groupArrayElements([1 , 2, 3, 4, 5], 3))
---
+++ README.md
@@ -0,0 +1,14 @@
+How To Install
+In the main folder
+
+npm install
+
+How To Run The Tests
+In the main folder
+
+npm test
+
+How To Run
+In the main folder
+
+npm start
